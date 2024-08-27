eccbe131a2aed1f7c258876accf76572d90309d4
Javaboiiii <devrajlokhande1610@gmail.com>
2024-08-27 22:51:25 +0530

    Implementation of the A-star

---
+++ research/graph_test3.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# create directed weighted graph
+# G = nx.DiGraph()
+# G.add_edge('A', 'B', weight=6)
+# G.add_edge('A', 'C', weight=2)
+# G.add_edge('C', 'B', weight=3)
+# G.add_edge('C', 'D', weight=1)
+# G.add_edge('B', 'D', weight=5)
+# G.add_edge('B', 'E', weight=2)
+# G.add_edge('D', 'E', weight=5)
+# G.add_edge('E', 'D', weight=4)
+
+# load graph from file
+G = nx.read_gexf('t_graph_test1.gexf')
+
+# find shortest path from D to E & its weight
+path = nx.shortest_path(G, source='E', target='D', weight='weight')
+length = nx.shortest_path_length(G, source='E', target='D', weight='weight')
+a_path = nx.astar_path(G, source='E', target='D', weight='weight')
+print('Shortest Path:', path)
+print('Shortest Path Length:', length)
+print('Shortest Path by A* is', a_path)
+
+# display directed graph
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=True, node_size=700, node_color='yellow')
+edge_labels = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+plt.show()
+
+# save the graph in a file
+nx.write_gexf(G, 't_graph_test1.gexf')
