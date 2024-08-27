0d44f97672caf603a103cd5214c2333cc9f3b1bc
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-08-27 22:40:26 +0530

    saveing & loading graph

--- research/graph_test1.py
+++ research/graph_test1.py
@@ -2,15 +2,18 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # create directed weighted graph
-G = nx.DiGraph()
-G.add_edge('A', 'B', weight=6)
-G.add_edge('A', 'C', weight=2)
-G.add_edge('C', 'B', weight=3)
-G.add_edge('C', 'D', weight=1)
-G.add_edge('B', 'D', weight=5)
-G.add_edge('B', 'E', weight=2)
-G.add_edge('D', 'E', weight=5)
-G.add_edge('E', 'D', weight=4)
+# G = nx.DiGraph()
+# G.add_edge('A', 'B', weight=6)
+# G.add_edge('A', 'C', weight=2)
+# G.add_edge('C', 'B', weight=3)
+# G.add_edge('C', 'D', weight=1)
+# G.add_edge('B', 'D', weight=5)
+# G.add_edge('B', 'E', weight=2)
+# G.add_edge('D', 'E', weight=5)
+# G.add_edge('E', 'D', weight=4)
+
+# load graph from file
+G = nx.read_gexf('t_graph_test1.gexf')
 
 # find shortest path from D to E & its weight
 path = nx.shortest_path(G, source='E', target='D', weight='weight')
@@ -24,3 +27,6 @@ nx.draw(G, pos, with_labels=True, node_size=700, node_color='yellow')
 edge_labels = nx.get_edge_attributes(G, 'weight')
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 plt.show()
+
+# save the graph in a file
+nx.write_gexf(G, 't_graph_test1.gexf')
